6888aeb318ffecf3ac6545635404799c4f14f0aa
unknown <mrao@MRAO-LT.nhsrx.com>
2016-03-23 12:14:22 -0500

    Added a rest point

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -1,7 +1,15 @@
 package com.springboot.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by mohanarao on 3/22/16.
  */
+@RestController
 public class HelloController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello world!";
+    }
 }
