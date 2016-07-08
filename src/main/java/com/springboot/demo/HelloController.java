65ffd7fefeb4bae1c867b2a5438c28cfa26c1012
mrao <mrao@pdxinc.com>
2016-07-08 09:03:01 -0500

    message one

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    public static final String MESSAGE = "Hello world !!!!";
+    public static final String MESSAGE = "Hai";
 
     @RequestMapping("/")
     public String home(){
